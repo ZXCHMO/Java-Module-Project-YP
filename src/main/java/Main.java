8a53d08957b227c356d25905582226dfe07a06f2
ZXCHMO <marat-ailarov@mail.ru>
2023-04-19 12:50:28 +0300

    zxc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Как тебя зовут?");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name + "!");
     }
 }
